ff23c730d2990983b01f8ceaf76b71c5ad9cf7d7
tuhuynhmobiclix <tu.huynh@mobiclixgroup.com>
2018-09-26 17:53:08 +0700

    Add send page

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,5 +1,6 @@
 import Login from '../app/modules/login/Login';
 import Home from '../app/modules/home/Home';
+import Send from '../app/modules/send/Send';
 
 export default [
     {
@@ -11,5 +12,10 @@ export default [
         path: "/login",
         title: "Login",
         component: Login
+    },
+    {
+        path: "/send",
+        title: "Send",
+        component: Send
     },
 ];
---
+++ src/app/modules/send/Send.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { Layout } from 'antd';
+
+const { Content } = Layout;
+
+class Send extends Component {
+    render() {
+        return (
+            <Content style={{ padding: '0 50px' }}>
+                <div style={{ background: '#fff', padding: '2rem', minHeight: 540 }}>
+                    Voluptate reprehenderit Lorem voluptate Lorem irure officia sunt. Qui aliquip pariatur sint in ipsum commodo officia Lorem sint dolore nisi. Sunt consequat occaecat excepteur et.
+                </div>
+            </Content>
+        );
+    }
+}
+
+export default Send;
