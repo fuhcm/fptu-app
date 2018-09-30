26747efac0a95d48d026942aec8424ed36b367e4
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-09-30 13:50:33 +0700

    Modify homepage

--- src/app/modules/home/Home.js
+++ src/app/modules/home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./Home.css";
+import Exception from "ant-design-pro/lib/Exception";
 
-import { Layout } from "antd";
+import { Layout, Button } from "antd";
 
 const { Content } = Layout;
 
@@ -16,12 +17,18 @@ class Home extends Component {
                         textAlign: "center",
                     }}
                 >
-                    <video controls className="home-video">
+                    {/* <video controls className="home-video">
                         <source
                             src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t66.18014-6/30642662_216616559196126_5297810338946876100_n.mp4?_nc_cat=109&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&oh=bef6c4f59283297955938e10164fd3ed&oe=5C237C4D"
                             type="video/mp4"
                         />
-                    </video>
+                    </video> */}
+                    <Exception
+                        type="500"
+                        actions={<Button type="primary">tới trang gửi confess</Button>}
+                        title="sad"
+                        desc="trang chủ giờ chưa có gì hết á, đi gửi confession nha?"
+                    />
                 </div>
             </Content>
         );
