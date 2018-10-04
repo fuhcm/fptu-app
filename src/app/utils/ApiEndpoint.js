aae87cd65d399032d31b02026578bc51ada5696a
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-10-04 11:25:52 +0700

    Update sw

--- src/app/utils/ApiEndpoint.js
+++ src/app/utils/ApiEndpoint.js
@@ -1,4 +1,4 @@
-export const BASE_URL      = "https://fptucf-api.gosu.team";
+export const BASE_URL      = "http://localhost:3000";
 export const AUTH_BASE_URL = BASE_URL + "/auth";
 export const API_BASE_URL  = BASE_URL + "/api/v1";
 
