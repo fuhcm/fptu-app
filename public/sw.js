4ecc07772254613dd9f87a0844f83951db2f53f4
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-12-09 19:10:06 +0700

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,8 @@
 var urlsToCache = [];
 
-urlsToCache.push("");
-
 urlsToCache.push("/");
 
-urlsToCache.push("/send");
-
-urlsToCache.push("/my-confess");
-
-urlsToCache.push("/login");
-
-urlsToCache.push("/admin-cp");
-
-var CACHE_NAME = "cache-cf-fptu-v1";
+var CACHE_NAME = "cache-cf-fptu-v2";
 
 self.addEventListener("install", function(event) {
     // Perform install steps
@@ -23,21 +13,15 @@ self.addEventListener("install", function(event) {
                 return cache.addAll(urlsToCache);
             })
             .catch(function(err) {
-                console.log("cache add err", err);
+                console.log("Cache add err", err);
             })
     );
 });
 
-// strategies from the offline cookbook by jake archibald
-// https://jakearchibald.com/2014/offline-cookbook/#serving-suggestions-responding-to-requests
-
 self.addEventListener("fetch", function(event) {
     event.respondWith(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return fetch(event.request).then(function(response) {
-                cache.put(event.request, response.clone());
-                return response;
-            });
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
         })
     );
 });
