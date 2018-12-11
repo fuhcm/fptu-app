42fa3ba02ed96ce48b81ca16ec919a6febf61d13
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-12-12 00:10:58 +0700

    update remove vn

--- src/app/modules/toidicodedao/Index.js
+++ src/app/modules/toidicodedao/Index.js
@@ -87,11 +87,11 @@ class Index extends Component {
             const patt = /p=(\d+)$/;
             const guid = patt.exec(post.guid)[1];
 
+            const postTitle = paramCase(this.removeVnStr(post.title));
+
             return (
                 <Link
-                    to={`/toidicodedao/bai-viet/${guid}/${paramCase(
-                        post.title
-                    )}`}
+                    to={`/toidicodedao/bai-viet/${guid}/${postTitle}`}
                     key={index}
                 >
                     <Col lg={8} md={12}>
@@ -126,6 +126,25 @@ class Index extends Component {
             );
         });
     };
+
+    removeVnStr(str) {
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+        str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+        str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+        str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+        str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+        str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+        str = str.replace(/Đ/g, "D");
+
+        return str;
+    }
 
     render() {
         const { loading, posts } = this.state;
