784d3101e4c4405b6f222cf52775f7518b8551a0
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-12-24 13:03:24 +0700

    add helmet

--- src/app/modules/review/Review.js
+++ src/app/modules/review/Review.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 
 import { Layout, Alert, Progress, Divider, Skeleton } from "antd";
 
+import { Helmet } from "react-helmet";
+
 const { Content } = Layout;
 
 class Review extends Component {
     render() {
         return (
             <Content className="content-container">
+                <Helmet>
+                    <title>Review môn học tại FPTU HCM</title>
+                </Helmet>
                 <div className="content-wrapper">
                     <h2>Đang xây dựng</h2>
                     <Progress percent={70} status="active" />
