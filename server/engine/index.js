e98be24270365739a612a19f53f71545ceeb4966
Tu Huynh <tu.huynh@mobiclixgroup.com>
2018-12-29 21:14:55 +0700

    remove hotjar tracking

--- server/engine/index.js
+++ server/engine/index.js
@@ -63,21 +63,6 @@ export default ({ html, preState, helmet, bundles }) => {
 
         gtag("config", "UA-123979019-2");
     </script>
-    <script>
-        (function(h, o, t, j, a, r) {
-            h.hj =
-                h.hj ||
-                function() {
-                    (h.hj.q = h.hj.q || []).push(arguments);
-                };
-            h._hjSettings = { hjid: 1126630, hjsv: 6 };
-            a = o.getElementsByTagName("head")[0];
-            r = o.createElement("script");
-            r.async = 1;
-            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-    </script>
 </body>
 </html>`;
 };
