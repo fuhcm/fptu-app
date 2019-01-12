607d1aacc7be73599643916763228e16c9efd52a
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-01-12 21:07:44 +0700

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 import reducers from "./app/reducers";
 
@@ -7,7 +8,7 @@ const initialState = {};
 
 export default function configureStore() {
     const middlewares = [thunk];
-    const enhancers = [applyMiddleware(...middlewares)];
+    const enhancers = [applyMiddleware(...middlewares, logger)];
     const store = createStore(reducers, initialState, compose(...enhancers));
 
     return store;
