c4ce00da93665a611e2355939b81bdc0d64b80f3
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-01-12 21:01:18 +0700

    add copyright to redux store

--- src/app/reducers/AuthReducer.js
+++ src/app/reducers/AuthReducer.js
@@ -1,6 +1,10 @@
 const initialState = {
-    logged : false,
-    isAdmin: false,
+    logged    : false,
+    isAdmin   : false,
+    appProfile: {
+        author : "Huynh Minh Tu",
+        address: "https://mrhmt.com",
+    },
 };
 
 export default (state = initialState) => {
