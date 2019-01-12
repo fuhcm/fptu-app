ca46560cd098d486170e447b7366dcb0f0176647
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-01-12 21:18:08 +0700

    not use redux logger on production

--- src/store.js
+++ src/store.js
@@ -8,7 +8,10 @@ const initialState = {};
 
 export default function configureStore() {
     const middlewares = [thunk];
-    const enhancers = [applyMiddleware(...middlewares, logger)];
+    const enhancers =
+        APP_ENV.NODE_ENV === "production"
+            ? [applyMiddleware(...middlewares)]
+            : [applyMiddleware(...middlewares, logger)];
     const store = createStore(reducers, initialState, compose(...enhancers));
 
     return store;
