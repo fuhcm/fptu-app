7dd95971041efef8d8ce8ef59f0e92c2a9ff863c
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-01-14 01:20:26 +0700

    update footer logo

--- src/app/modules/footer/Footer.js
+++ src/app/modules/footer/Footer.js
@@ -33,46 +33,61 @@ class FooterPage extends PureComponent {
                         src="/assets/images/icons/golang.png"
                         width="50px"
                         alt="golang"
+                        title="Golang"
                     />
                     <img
                         src="/assets/images/icons/nodejs.png"
                         width="50px"
                         alt="nodejs"
+                        title="NodeJS"
                     />
                     <img
                         src="/assets/images/icons/react.png"
                         width="50px"
                         alt="reactjs"
+                        title="ReacJS"
                     />
                     <img
                         src="/assets/images/icons/mysql.png"
                         width="50px"
                         alt="mysql"
+                        title="MySQL"
                     />
                     <img
                         src="/assets/images/icons/docker.png"
                         width="50px"
                         alt="docker"
+                        title="Docker"
                     />
                     <img
                         src="/assets/images/icons/gcp.png"
                         width="50px"
                         alt="gcp"
+                        title="Google Cloud Platform"
                     />
                     <img
                         src="/assets/images/icons/kubernetes.png"
                         width="50px"
                         alt="kubernetes"
-                    />
-                    <img
-                        src="/assets/images/icons/elastic.png"
-                        width="50px"
-                        alt="elastic"
+                        title="Kubernetes"
                     />
                     <img
                         src="/assets/images/icons/jenkins.png"
                         width="50px"
                         alt="jenkins"
+                        title="Jenkins"
+                    />
+                    <img
+                        src="/assets/images/icons/elastic.png"
+                        width="50px"
+                        alt="elastic"
+                        title="Elastic Search"
+                    />
+                    <img
+                        src="/assets/images/icons/grafana.png"
+                        width="50px"
+                        alt="grafana"
+                        title="Grafana"
                     />
                 </div>
             </Footer>
