fb81b2adc9ae245ef69aba942063a4a95988ca87
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-01-24 01:43:11 +0700

    improve SSR

--- server/render/index.js
+++ server/render/index.js
@@ -5,6 +5,9 @@ import { Provider } from "react-redux";
 import { HelmetProvider } from "react-helmet-async";
 import Loadable from "react-loadable";
 import { getBundles } from "react-loadable/webpack";
+import { trigger } from "redial";
+import axios from "axios";
+import striptags from "striptags";
 import Engine from "../engine";
 import App from "../../src/app/App";
 
@@ -20,7 +23,8 @@ class Renderer {
         app.get("*", this.renderApp(store));
     }
 
-    renderApp = store => (req, res) => {
+    renderApp = store => async (req, res) => {
+        trigger();
         let context = {};
         let helmetContext = {};
         let modules = [];
@@ -39,6 +43,39 @@ class Renderer {
 
         const preState = store.getState();
         let bundles = getBundles(stats, modules);
+
+        if (req.originalUrl.includes("/fpt/") || req.originalUrl.includes("/medium/") || req.originalUrl.includes("/toidicodedao/")) {
+            let feedName = req.originalUrl.split("/")[1];
+            let articleID = req.originalUrl.split("/")[2];
+
+            if (feedName === "toidicodedao") { feedName = "codedao"; articleID = req.originalUrl.split("/")[3];  }
+
+            const { data } = await axios.get(
+                "https://api.fptu.tech/crawl/" + feedName + "/" + articleID
+            );
+
+            const shortDesc = striptags(data.description).substring(0, 250) || "FPTU dot Tech" + "...";
+
+            helmetContext.helmet.title = {
+                toComponent: function() {
+                    return null;
+                },
+                toString: function() {
+                    return `<title data-rh="true">${data.title || "FPTU dot Tech"}</title>`;
+                },
+            };
+
+            helmetContext.helmet.meta = {
+                toComponent: function() {
+                    return null;
+                },
+                toString: function() {
+                    return `<meta property="og:type" content="article" /><meta property="og:description" content="${shortDesc}" /><meta property="og:image" content="${
+                        data.thumbnail || "https://cdn-images-1.medium.com/max/1024/1*hMHI6laZkdZMdZNnSQg5AA.jpeg"
+                    }" />`;
+                },
+            };
+        }
 
         res.send(
             Engine({
