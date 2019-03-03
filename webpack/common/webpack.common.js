085776ec7d112af6f6c5f3ac87db6cb0711ae879
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-03-03 15:37:52 +0700

    remove uglify and replaced by teaser

--- webpack/common/webpack.common.js
+++ webpack/common/webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
@@ -103,10 +103,9 @@ module.exports.optimization = {
         },
     },
     minimizer: [
-        new UglifyJsPlugin({
-            cache    : true,
-            parallel : true,
-            sourceMap: false,
+        new TerserPlugin({
+            cache   : true,
+            parallel: true,
         }),
         new OptimizeCSSAssetsPlugin({}),
     ],
