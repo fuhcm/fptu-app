36816346b0bcf2ab18b260fbf9f34012ae634c38
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-03-22 02:37:17 +0700

    update caches

--- public/sw.js
+++ public/sw.js
@@ -4,12 +4,48 @@ importScripts(
 );
 
 if (workbox) {
-    // Force development builds
-    workbox.setConfig({ debug: true });
+    // Force production builds
+    workbox.setConfig({ debug: false });
 
     workbox.routing.registerRoute("/", new workbox.strategies.NetworkFirst());
     workbox.routing.registerRoute(
         "/send",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        "/my-confess",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        "/search",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        "/login",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        "/medium",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        "/toidicodedao",
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        new RegExp(".+\\.js$"),
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        new RegExp(".+\\.css$"),
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        new RegExp(".+\\.jpg$"),
+        new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+        new RegExp(".+\\.png$"),
         new workbox.strategies.NetworkFirst()
     );
 } else {
