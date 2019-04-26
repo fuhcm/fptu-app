787170d327276a86808858315b9d9d8d2d3738f5
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-04-26 14:37:43 +0700

    remove slack integration

--- src/app/modules/send/Send.js
+++ src/app/modules/send/Send.js
@@ -17,7 +17,6 @@ import { ReCaptcha } from "react-recaptcha-google";
 import firebase from "firebase/app";
 import "firebase/storage";
 import FileUploader from "react-firebase-file-uploader";
-import axios from "axios";
 
 const { Content } = Layout;
 const { TextArea } = Input;
@@ -83,34 +82,8 @@ class Send extends Component {
                                 2.5
                             )
                         );
-
-                    axios.post(
-                        "https://hooks.slack.com/services/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",
-                        {
-                            text: `${contentTextarea}`,
-                        },
-                        {
-                            headers: {
-                                "Content-Type":
-                                    "application/x-www-form-urlencoded",
-                            },
-                        }
-                    );
                 } else {
                     message.error("Lỗi kết nối nên chưa gửi được");
-
-                    axios.post(
-                        "https://hooks.slack.com/services/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",
-                        {
-                            text: `Có lỗi hệ thống nên chưa gửi được confess! :smiley: Nội dung confess đó là: ${contentTextarea}`,
-                        },
-                        {
-                            headers: {
-                                "Content-Type":
-                                    "application/x-www-form-urlencoded",
-                            },
-                        }
-                    );
 
                     this.setState({
                         disabledSendButton: false,
