6ac01214f6c6f7e8572fd50b6181b169301f7d99
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-05-01 23:53:14 +0700

    these confounded tests drive me nuts

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import Loadable from "react-loadable";
 import { loadReCaptcha } from "react-recaptcha-google";
-import Loading from "./modules/loading/Loading";
 import App from "./App";
-
-const LandingPage = Loadable({
-    loader: () =>
-        import(/* webpackChunkName: "landingPage" */ "./modules/landing-page/LandingPage"),
-    loading: () => <Loading />,
-    modules: ["../app/modules/landing-page/LandingPage"],
-    webpack: () => [
-        require.resolveWeak("../app/modules/landing-page/LandingPage"),
-    ],
-});
+import LandingPage from "./modules/landing-page/LandingPage";
 
 class Main extends Component {
     componentDidMount() {
