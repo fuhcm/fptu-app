3135a760a85457f7f4d0404876676262e68c475d
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-05-02 00:39:36 +0700

    put code that worked where the code that didn't used to be

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { loadReCaptcha } from "react-recaptcha-google";
 import App from "./App";
 import LandingPage from "./modules/landing-page/LandingPage";
@@ -17,19 +17,6 @@ class Main extends Component {
                     <Route path="/" exact component={LandingPage} />
                     <Route path="/*" component={App} />
                 </Switch>
-
-                <Link to="/medium">
-                    <img
-                        alt="Gopher Icon"
-                        src="/assets/images/gopher.svg"
-                        style={{
-                            maxWidth: "70px",
-                            position: "fixed",
-                            bottom  : "10px",
-                            right   : "10px",
-                        }}
-                    />
-                </Link>
             </React.Fragment>
         );
     }
