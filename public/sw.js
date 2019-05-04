f600f8e4e12a43395a373dc6afe949dff58d1517
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-05-04 18:02:38 +0700

    whooooooooooooooooooooooooooo

--- public/sw.js
+++ public/sw.js
@@ -7,66 +7,69 @@ if (workbox) {
     // Force production builds
     workbox.setConfig({ debug: false });
 
-    workbox.routing.registerRoute("/", new workbox.strategies.NetworkFirst());
+    workbox.routing.registerRoute(
+        "/",
+        new workbox.strategies.StaleWhileRevalidate()
+    );
     workbox.routing.registerRoute(
         "/home",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/send",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/my-confess",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/search",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/login",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/medium",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/toidicodedao",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         "/radio",
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.js$"),
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.css$"),
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.jpg$"),
-        new workbox.strategies.CacheFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.png$"),
-        new workbox.strategies.CacheFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.ico$"),
-        new workbox.strategies.CacheFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.json$"),
-        new workbox.strategies.NetworkFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
     workbox.routing.registerRoute(
         new RegExp(".+\\.woff2$"),
-        new workbox.strategies.CacheFirst()
+        new workbox.strategies.StaleWhileRevalidate()
     );
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
