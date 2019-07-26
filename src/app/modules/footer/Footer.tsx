e97ce5d417a8a1b653ddc94192c0bf5cbb1c8cc5
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-07-26 16:50:10 +0700

    I must enjoy torturing myself

--- src/app/modules/footer/Footer.tsx
+++ src/app/modules/footer/Footer.tsx
@@ -43,64 +43,7 @@ class FooterPage extends Component<Props> {
                             Huynh Minh Tu
                         </a>
                     </strong>
-                    . Built on top of Go & React, hosted at Heroku & Netlify.
-                </div>
-
-                <div style={{ marginTop: "10px" }}>
-                    <img
-                        src="/assets/images/icons/golang.png"
-                        width="50px"
-                        alt="golang"
-                        title="Golang"
-                    />
-                    <img
-                        src="/assets/images/icons/typescript.png"
-                        width="50px"
-                        alt="typescript"
-                        title="TypeScript"
-                    />
-                    <img
-                        src="/assets/images/icons/nodejs.png"
-                        width="50px"
-                        alt="nodejs"
-                        title="Node.js"
-                        style={{
-                            margin: "0px 5px",
-                        }}
-                    />
-                    <img
-                        src="/assets/images/icons/react.png"
-                        width="50px"
-                        alt="react"
-                        title="React"
-                    />
-                    <img
-                        src="/assets/images/icons/mysql.png"
-                        width="50px"
-                        alt="mysql"
-                        title="MySQL"
-                        style={{
-                            margin: "0px 5px",
-                        }}
-                    />
-                    <img
-                        src="/assets/images/icons/docker.png"
-                        width="50px"
-                        alt="docker"
-                        title="Docker"
-                    />
-                    <img
-                        src="/assets/images/icons/heroku.png"
-                        width="50px"
-                        alt="heroku"
-                        title="Heroku"
-                    />
-                    <img
-                        src="/assets/images/icons/netlify.png"
-                        width="50px"
-                        alt="netlify"
-                        title="Netlify"
-                    />
+                    . Built on top of Go & React, hosted at Google Cloud Platform.
                 </div>
             </Footer>
         );
