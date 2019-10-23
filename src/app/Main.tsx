59a0b7b030307d77735ca5447e163572c8610d7c
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-10-23 18:24:08 +0700

    fix /sigh

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { loadReCaptcha } from "react-recaptcha-google";
 import App from "./App";
 import LandingPage from "./modules/landing-page/LandingPage";
 
-class Main extends Component<{}> {
-    componentDidMount() {
-        // Load captcha
+function Main() {
+    useEffect(() => {
         loadReCaptcha();
-    }
+    });
 
-    render() {
-        return (
-            <React.Fragment>
-                <Switch>
-                    <Route path="/" exact component={LandingPage} />
-                    <Route path="/*" component={App} />
-                </Switch>
-            </React.Fragment>
-        );
-    }
+    return (
+        <React.Fragment>
+            <Switch>
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/*" component={App} />
+            </Switch>
+        </React.Fragment>
+    );
 }
 
 export default Main;
