441cc3f86acc53417fef937e48feda6aa2d20459
Tu Huynh <tu.huynh@linecorp.com>
2019-11-27 16:03:35 +0700

    I was told to leave it alone, but I have this thing called OCD, you see

---
+++ src/app/Main.spec.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+
+describe("test main", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<Main />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
